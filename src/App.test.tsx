6043fef1ec072208a6883f76cbf75dcf0641f845
vincentpresentation12 <vincent.crosnier12@gmail.com>
2023-03-14 03:11:47 +0100

    finition todo app front

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -9,6 +9,7 @@ test("renders learn react link", () => {
       <App />
     </BrowserRouter>
   );
+
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
